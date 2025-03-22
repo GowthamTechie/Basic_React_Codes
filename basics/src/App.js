a32d399107f042b171796ba0abd30be3c10655f5
GowthamTechie <tadiboinagowtham2001@gmail.com>
2025-03-22 17:21:47 +0530

    Upto Greeting Component

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,23 @@
 
+// import About from "./About";
+// import Contact from "./Contact";
+
+import Greetings from "./Greetings";
+
+// import Home from "./Home";
 function App() {
+ 
   return (
-    <>Hello Gowtham Welcome to react</>
+    <>
+    <h1>Hello Gowtham Welcome to React Project</h1>
+    {/* <Home name ={'Gowtham'} age = {24}/>
+    <About/>
+    <Contact/> */}
+    <Greetings name={'Gowtham'}/>
+    <Greetings name={'Lokesh'}/>
+    <Greetings name={'Srinivas'}/>
+
+    </>
   );
 }
 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <>
+        <h1>Header Component</h1>
+        <h2>This is from Header Component</h2>
+        </>
+    )
+
+}
+export default Header;
